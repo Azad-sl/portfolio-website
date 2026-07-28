b9d92ee2d7288ea323e8f2f0fcb5f2d20925f2e4
Azad-sl <85327306+Azad-sl@users.noreply.github.com>
2026-07-28 16:59:56 +0800

    Update HelpPrompt.tsx

--- src/Application/UI/components/HelpPrompt.tsx
+++ src/Application/UI/components/HelpPrompt.tsx
@@ -2,16 +2,16 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 // import eventBus from '../EventBus';
 import { motion } from 'framer-motion';
 import UIEventBus from '../EventBus';
-
-const HELP_TEXT = 'Click anywhere to begin';
-
+ 
+const HELP_TEXT = '点击任意位置开始';
+ 
 type HelpPromptProps = {};
-
+ 
 const HelpPrompt: React.FC<HelpPromptProps> = () => {
     const [helpText, setHelpText] = useState('');
     const [visible, setVisible] = useState(true);
     const visRef = useRef(visible);
-
+ 
     const typeHelpText = (i: number, curText: string) => {
         if (i < HELP_TEXT.length && visRef.current) {
             setTimeout(() => {
@@ -19,15 +19,15 @@ const HelpPrompt: React.FC<HelpPromptProps> = () => {
                     { type: 'keydown', key: `_AUTO_${HELP_TEXT[i]}` },
                     '*'
                 );
-
+ 
                 setHelpText(curText + HELP_TEXT[i]);
                 typeHelpText(i + 1, curText + HELP_TEXT[i]);
-            }, Math.random() * 120 + 50);
+            }, Math.random() * 150 + 80);
         }
     };
-
+ 
     // make a document listener to listen to clicks
-
+ 
     useEffect(() => {
         setTimeout(() => {
             typeHelpText(0, '');
@@ -39,14 +39,14 @@ const HelpPrompt: React.FC<HelpPromptProps> = () => {
             setVisible(false);
         });
     }, []);
-
+ 
     useEffect(() => {
         if (visible == false) {
             window.postMessage({ type: 'keydown', key: `_AUTO_` }, '*');
         }
         visRef.current = visible;
     }, [visible]);
-
+ 
     return helpText.length > 0 ? (
         <motion.div
             variants={vars}
@@ -62,7 +62,7 @@ const HelpPrompt: React.FC<HelpPromptProps> = () => {
         <></>
     );
 };
-
+ 
 const vars = {
     visible: {
         opacity: 1,
@@ -76,7 +76,7 @@ const vars = {
         },
     },
 };
-
+ 
 const styles: StyleSheetCSS = {
     container: {
         position: 'absolute',
@@ -90,12 +90,10 @@ const styles: StyleSheetCSS = {
         alignItems: 'flex-end',
     },
     blinkingContainer: {
-        // width: 100,
-        // height: 100,
         marginLeft: 8,
         paddingBottom: 2,
         paddingRight: 4,
     },
 };
-
+ 
 export default HelpPrompt;
